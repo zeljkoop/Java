0370658bd34aee791c7c6731b22fe9f273a5e7bf
teko <teko@MacBook-Air-von-admin.local>
2022-10-28 11:26:55 +0200

    Folienaufgabe 1 + Zusatzaufgabe.

--- folienaufgabe_1.java
+++ folienaufgabe_1.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class folienaufgabe_1
 {
     public static void main(String[] args) {
-        System.out.println("hallo Welt");
+        String Eingabe1 = new java.util.Scanner(System.in).nextLine();
+        String Eingabe2 = new java.util.Scanner(System.in).nextLine();
+        float Wert1 = Float.parseFloat(Eingabe1);
+        float Wert2 = Float.parseFloat(Eingabe2);
+        System.out.println(Wert1 + Wert2);
+        int ganzzahl = (int)Wert1;
+        int ganzzahl2 = (int)Wert2;
+        System.out.println(ganzzahl + ganzzahl2);
     }
 }
