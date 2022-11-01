e557b10ba99852a8d66ab792809c9dbdb8b07204
Zeljko <z.pajkic@zhbsd.ch>
2022-11-01 09:47:48 +0100

    Sekunden_Umrechnen.java fertiggestellt

--- Aufgaben/Sekunden_Umrechnen.java
+++ Aufgaben/Sekunden_Umrechnen.java
@@ -2,21 +2,14 @@ import java.util.*;
 public class Sekunden_Umrechnen {
 public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input seconds: ");
-	    	int seconds = in.nextInt();
-        int S = seconds % 60;
-        int H = seconds / 60;
-        int M = H % 60;
-        H = H / 60;
-        System.out.print( H + ":" + M + ":" + S);
-		System.out.print("\n");
+        String eingabe;
+        eingabe = new java.util.Scanner(System.in).nextLine();
+        int sekundenEingabe = Integer.parseInt(eingabe);
+        int stunden = sekundenEingabe / 3600;
+        int minuten = (sekundenEingabe % 3600) / 60;
+        int sekunden = sekundenEingabe % 60;
+
+        System.out.println(stunden + ":" + minuten + ":" + sekunden);
+
     }
  }
-
-/**
- * Schreiben Sie ein Java-Programm, das eine Anzahl von Sekunden einliest und in die Anzahl von Stunden, 
- * Minuten und Sekunden umrechnet und ausgibt. Die Eingabe 1234 soll also zur Ausgabe 0:20:34 führen. 
- * Testen Sie Ihr Programm mit vernünftigen Eingaben, zum Beispiel: 0, 59, 60, 100, 3600 und 4000.
- * Zeichnen Sie als Unterstützung zuerst ein Flussdiagramm.
- */
